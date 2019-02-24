dc8fe6e1f6bd5dd5aab317fed94becec7d319252
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-02-24 22:38:41 +0100

    test js2

--- app/assets/javascripts/animation/onclick.js
+++ app/assets/javascripts/animation/onclick.js
@@ -1,3 +1,3 @@
-var menu_appear = TweenMax.fromTo(".menu", 0,{ x:"-100%", y:"-50%",opacity:0}, {x:"0%",y:"-50%",opacity:1, ease: Power1.easeInOut});
+var menu_appear = TweenMax.fromTo(".menu", 1,{ x:"-100%", y:"0%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power1.easeInOut});
 
 $('#appear').click( function(){menu_appear});
