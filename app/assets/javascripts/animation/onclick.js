03c5aec044ce3587750af880fa3e7fd68b3a8c4e
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-02-24 19:00:01 +0100

    fix cloudinary

--- app/assets/javascripts/animation/onclick.js
+++ app/assets/javascripts/animation/onclick.js
@@ -1,10 +1,3 @@
+var menu_appear = TweenMax.fromTo(".menu", 0,{ x:"-100%", y:"-50%",opacity:0}, {x:"0%",y:"-50%",opacity:1, ease: Power1.easeInOut});
 
-$("#appear").click(function menu_appear(){
-    $(".sayhello").hide(),
-    $(".menu").show();
-    // TweenMax.fromTo($(this).find(".all_detail"), 0,{ x:"-50%", y:"-50%",opacity:0}, {x:"-2%",y:"-25%",opacity:1, ease: Power1.easeInOut})
-    // TweenMax.fromTo((".menu"), 1,{autoAlpha:0}, { autoAlpha:0, ease: Power1.easeInOut});
-console.log(menu_appear);
-});
-
-
+$('#appear').click( function(){menu_appear});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+
 
 
 
