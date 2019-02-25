b12ac778e0951dfa5ed9ac6aeeec87094e2b82b2
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-02-25 14:09:41 +0100

    delete signup admin

--- app/assets/javascripts/animation/onclick.js
+++ app/assets/javascripts/animation/onclick.js
@@ -1,3 +1,3 @@
-var menu_appear = TweenMax.fromTo(".menu", 1,{ x:"-100%", y:"0%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power1.easeInOut});
+function menu_appear(){TweenMax.fromTo(".menu", 0.5,{ x:"-100%", y:"0%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power1.easeInOut});} 
 
-$('#appear').click( function(){menu_appear});
+$('#appear').click(menu_appear);
