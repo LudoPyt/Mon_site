dd6cb950a7c3720d72babd6e342c8e15f19c3dae
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-02-25 14:55:18 +0100

    repair typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  
+  #compile fonts
+  config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
--- public/javascripts/animation/onclick.js
+++
@@ -1,3 +0,0 @@
-function menu_appear(){TweenMax.fromTo(".menu", 0.5,{ x:"-100%", y:"0%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power1.easeInOut});} 
-
-$('#appear').click(menu_appear);
