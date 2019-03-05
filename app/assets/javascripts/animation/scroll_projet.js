4d55afb9d205a599f09f6709479046d043cbc597
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-03-05 09:18:23 +0100

    correct text

--- app/assets/javascripts/animation/scroll_projet.js
+++ app/assets/javascripts/animation/scroll_projet.js
@@ -5,9 +5,11 @@ $('.content').each(function(){
     var scroll_anim = new TimelineMax();
 
     scroll_anim.add([
-        TweenMax.fromTo($(this).find(".all_img"), 1,{x:"-50%", y:"-50%"}, { x:"-98%", y:"-75%",ease: Power1.easeInOut}),
+        TweenMax.fromTo($(this).find(".all_img"), 1,{x:"-50%", y:"-50%"},
+         { x:"-98%", y:"-75%",ease: Power1.easeInOut}),
 
-        TweenMax.fromTo($(this).find(".all_detail"), 1,{ x:"-50%", y:"-50%",opacity:0}, {x:"-2%",y:"-25%",opacity:1, ease: Power1.easeInOut})
+        TweenMax.fromTo($(this).find(".all_detail"), 1,{ x:"-50%", y:"-50%",opacity:0},
+         {x:"-2%",y:"-25%",opacity:1, ease: Power1.easeInOut})
     ])
 
     var scroll = new ScrollMagic.Scene({
