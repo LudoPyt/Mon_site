f4e1a1391a2d1cd180aac4d2830504c46e950dd4
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-03-20 15:40:09 +0100

    change field apropos

--- app/assets/javascripts/animation/onclick.js
+++ app/assets/javascripts/animation/onclick.js
@@ -1,9 +1,8 @@
 function menuAppear(){
-
+  TweenMax.to(".menu", .1, {autoAlpha:1, ease: Power1.easeOut}),
   TweenMax.fromTo(".detail1", 0.9,{ x:"0%", y:"-200%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power4.easeInOut}),
   TweenMax.fromTo(".detail2", 0.7,{ x:"0%", y:"-200%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power4.easeInOut}),
   TweenMax.fromTo(".detail3", 0.5,{ x:"0%", y:"-200%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power4.easeInOut}),
-  TweenMax.to(".menu", .1, {autoAlpha:1, ease: Power1.easeOut}),
   TweenMax.to(".sayhello", .1, {display: "none", ease: Power1.easeOut}),
   TweenMax.to("#a_propos", .1, {display: "none", ease: Power1.easeOut}),
   TweenMax.to("#contact", .1, {display: "none", ease: Power1.easeOut}),
