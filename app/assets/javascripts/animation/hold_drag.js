9b5629cfeb5bbf357eb7cb616c11ec4c8d12a5b5
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-03-20 15:07:25 +0100

    add anim menuhome

--- app/assets/javascripts/animation/hold_drag.js
+++ app/assets/javascripts/animation/hold_drag.js
@@ -19,8 +19,8 @@
 
 function Appear() {
 
-  let mymenu = new TimelineMax();
-  mymenu.add([
+  let boum = new TimelineMax();
+  boum.add([
     TweenMax.to(".cursor", .2, {display: "none", ease: Power1.easeOut}),
     TweenMax.fromTo(".det1", 0.5,{ x:"0%", y:"200%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power4.easeInOut}),
     TweenMax.fromTo(".det2", 0.7,{ x:"0%", y:"200%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power4.easeInOut}),
