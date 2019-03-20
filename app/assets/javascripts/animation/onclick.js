d8b9860d0489a0e9657f7934cbc50fa5b76170a0
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-03-20 16:11:13 +0100

    fix btn menu

--- app/assets/javascripts/animation/onclick.js
+++ app/assets/javascripts/animation/onclick.js
@@ -1,15 +1,20 @@
 function menuAppear(){
 
-  TweenMax.to(".menu", .1, {autoAlpha:1}),
-  TweenMax.fromTo(".detail1", 0.9,{ x:"0%", y:"-200%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power4.easeInOut}),
-  TweenMax.fromTo(".detail2", 0.7,{ x:"0%", y:"-200%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power4.easeInOut}),
-  TweenMax.fromTo(".detail3", 0.5,{ x:"0%", y:"-200%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power4.easeInOut}),
-  TweenMax.to(".sayhello", .1, {display: "none", ease: Power1.easeOut}),
-  TweenMax.to("#a_propos", .1, {display: "none", ease: Power1.easeOut}),
-  TweenMax.to("#contact", .1, {display: "none", ease: Power1.easeOut}),
-  TweenMax.to("#all_projet", .1, {display: "none", ease: Power1.easeOut}),
-  TweenMax.to("#details", .1, {display: "none", ease: Power1.easeOut}),
-  TweenMax.to(".ariane", .1, {display: "none", ease: Power1.easeOut});
+
+   let menuClick = new TimelineMax();
+    menuClick.add([
+    TweenMax.to(".menu", .1, {autoAlpha:1, ease: Power1.easeOut}),
+    TweenMax.fromTo(".detail1", 0.9,{ x:"0%", y:"-200%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power4.easeInOut}),
+    TweenMax.fromTo(".detail2", 0.7,{ x:"0%", y:"-200%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power4.easeInOut}),
+    TweenMax.fromTo(".detail3", 0.5,{ x:"0%", y:"-200%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power4.easeInOut}),
+    TweenMax.to(".sayhello", .1, {display: "none", ease: Power1.easeOut}),
+    TweenMax.to("#a_propos", .1, {display: "none", ease: Power1.easeOut}),
+    TweenMax.to("#contact", .1, {display: "none", ease: Power1.easeOut}),
+    TweenMax.to("#all_projet", .1, {display: "none", ease: Power1.easeOut}),
+    TweenMax.to("#details", .1, {display: "none", ease: Power1.easeOut}),
+    TweenMax.to(".ariane", .1, {display: "none", ease: Power1.easeOut})
+    ])
+
 }
 
 $('#appear').click(menuAppear);
