b93e9cd0a443fbf336cca6a24305be5d03fc87b1
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-03-26 11:35:10 +0100

    anim menu smooth

--- app/assets/javascripts/animation/btnMenu.js
+++ app/assets/javascripts/animation/btnMenu.js
@@ -3,7 +3,7 @@ function menuAppear(){
 
    let menuClick = new TimelineMax();
     menuClick.add([
-    TweenMax.staggerFromTo(".menu_detail_home", 1, { x:"0%", y:"-150%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power2.easeInOut}, -.3),
+    TweenMax.staggerFromTo(".menu_detail_home", 1, { x:"0%", y:"-150%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power2.easeInOut}, -.07),
     TweenMax.to(".sayhello", .1, {display: "none", ease: Power1.easeOut}),
     TweenMax.to("#a_propos", .1, {display: "none", ease: Power1.easeOut}),
     TweenMax.to("#contact", .1, {display: "none", ease: Power1.easeOut}),
