35b3b39511abe164c2e702bebf254930e96d3c44
Pouyaut Ludovic <ludovic.pouyaut@gmail.com>
2019-03-26 10:18:32 +0100

    fix menu

--- app/assets/javascripts/animation/onclick.js
+++ app/assets/javascripts/animation/onclick.js
@@ -3,7 +3,6 @@ function menuAppear(){
 
    let menuClick = new TimelineMax();
     menuClick.add([
-    // TweenMax.fromTo("#menu", .1,{autoAlpha:0}, {autoAlpha:1, ease: Power1.easeInOut}),
     TweenMax.staggerFromTo(".menu_detail_home", 1, { x:"0%", y:"-150%",autoAlpha:0}, {x:"0%",y:"0%",autoAlpha:1, ease: Power2.easeInOut}, -.3),
     TweenMax.to(".sayhello", .1, {display: "none", ease: Power1.easeOut}),
     TweenMax.to("#a_propos", .1, {display: "none", ease: Power1.easeOut}),
